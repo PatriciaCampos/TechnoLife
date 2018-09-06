a97747d444661327eb949c864da0e35e432c99f9
juanpenita1997 <42654560+juanpenita1997@users.noreply.github.com>
2018-09-06 16:03:59 -0300

    Delete SP_ACTUALIZAR_TIPO_USUARIO.sql

--- SP_ACTUALIZAR_TIPO_USUARIO.sql
+++
@@ -1,9 +0,0 @@
-CREATE PROCEDURE SP_ACTUALIZAR_TIPO_USUARIO
-(
- @id_tipo     INTEGER,
- @nombre_tipo VARCHAR(30)
-) 
-AS
-UPDATE tipo_usuario SET nombre_tipo = @nombre_tipo
-WHERE id_tipo = @id_tipo
-GO
